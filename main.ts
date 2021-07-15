2e327d20a0a71c9942689688b56363e4e18ea50d
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2021-07-15 07:07:17 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,21 @@
+let accY = 0
+let accX = 0
+let x = 2
+let y = 2
+basic.forever(function () {
+    led.plot(x, y)
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+    if (accX < -150 && x > 0) {
+        x += -1
+    } else if (accX > 150 && x < 4) {
+        x += 1
+    }
+    if (accY < -150 && y > 0) {
+        y += -1
+    } else if (accY > 150 && y < 4) {
+        y += 1
+    }
+    basic.pause(500)
+    basic.clearScreen()
+})
